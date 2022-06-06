b063c59f1ee647d16a1744a9333bb9db5ad09547
NikitaShevchik <shevchik.nikita@gmail.com>
2022-06-06 15:20:44 +0300

    closure

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,5 +1,5 @@
 "use strict"
-
+/*---------
 function hello() {
     console.log('Hello', this);
 }
@@ -41,7 +41,79 @@ Array.prototype.multBy = function (n) {
         return i * n;
     })
 }
-
+-----------*/
 // console.log(array.multBy(6));
 
 
+// function createCalc(n) {
+//     return function() {
+//         console.log(1000 * n);
+//     }
+// }
+
+// const calc = createCalc(33);
+// calc()
+
+
+/*--------
+function createInc(n) {
+    return function (num) {
+        return n + num;
+    }
+}
+const addOne = createInc(1);
+const addTen = createInc(10);
+
+console.log(addOne(9));
+console.log(addOne(19));
+
+console.log(addTen(9));
+console.log(addTen(19));
+
+
+function urlCreator(domain){
+    return function(url) {
+        return `https://${url}.${domain}`
+    }
+}
+
+const com = urlCreator('com');
+
+console.log(com('youtube'))
+--------*/
+
+function logPerson() {
+    console.log(`Person: ${this.name}, ${this.age}, ${this.job}`)
+}
+const personOne = {
+    name: 'Misha',
+    age: 22,
+    job: 'Hoe'
+}
+const personTwo = {
+    name: 'Alex',
+    age: 44,
+    job: 'Homeless'
+}
+
+// logPerson.bind(personOne)()
+
+function bind(pers, fn) {
+    return function (...args) {
+        fn.apply(pers, args)
+    }
+}
+
+bind(personOne, logPerson)();
+bind(personTwo, logPerson)();
+
+// binde(personOne)();
+// binde(personTwo)();
+
+
+
+// const first = binde(logPerson);
+// console.log(first(personOne))
+
+// bind(personOne, logPerson);
+// bind(personTwo, logPerson);
