e6453ee14e0c33daeb09b327bf909ce309adb5ec
NikitaShevchik <shevchik.nikita@gmail.com>
2022-06-06 14:01:00 +0300

    this+bind+console.group

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,1 +1,29 @@
 "use strict"
+
+function hello() {
+    console.log('Hello', this);
+}
+
+const person = {
+    name: 'Nikita',
+    age: 22,
+    sayHello: hello,
+    sayHelloWindow: hello.bind(this),
+    logInfo: function (job, phone) {
+        console.group(`${this.name} info:`)
+        console.log(`Name is ${this.name}`)
+        console.log(`Age is ${this.age}`)
+        console.log(`Job is ${job}`)
+        console.log(`Phone is ${phone}`)
+        console.groupEnd()
+    }
+}
+
+const lena = {
+    name: 'Elena',
+    age: 15
+}
+
+const fnLenaInfoLog = person.logInfo.bind(lena);
+fnLenaInfoLog('Frontend', 'IPhone')
+person.logInfo('Dealer', 'Android')
